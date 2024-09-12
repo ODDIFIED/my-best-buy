b2d912e31cdd8dbb4f0114342bf1c320ffa15554
DAVID CHISOM <iamdheevee@gmail.com>
2024-09-12 16:20:47 +0100

    second commit

--- src/component/HomeFolder/Navbar/Hero.jsx
+++ src/component/HomeFolder/Navbar/Hero.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <div className=" ">
-      <div class=" w-full container sm:px-10 py-10 mx-auto  bg-gray-200   dark:bg-gray-900 dark:text-white ">
+      <div class="  sm:px-10 py-10 mx-auto  bg-gray-200   dark:bg-gray-900 dark:text-white ">
         <div className=" mt-20  w-full sm:flex sm:flex-row sm:justify-between xxs:items-center sm:gap-10 xxs:flex-col ">
           <motion.div
             initial={{ x: "-250vw" }}
